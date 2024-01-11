076c16f8540f0d5319e450a2c750ffe8475254f4
Mabende <belindamabende@gmail.com>
2024-01-11 00:00:41 -0800

    updated

---
+++ betty.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * custom_hello - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int custom_hello(void)
+{
+	const char message[] = "Hello, world!\n";
+
+	write(1, message, sizeof(message) - 1);
+	return (0);
+}
+
